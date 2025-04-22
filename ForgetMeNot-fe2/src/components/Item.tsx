81d29a92b9066d115a797424b57b7973ea9cbd88
Antonia Amosova <amosova1@uniba.sk>
2025-04-22 22:46:16 +0200

    Fix displaying svg

--- ForgetMeNot-fe2/src/components/Item.tsx
+++ ForgetMeNot-fe2/src/components/Item.tsx
@@ -10,7 +10,7 @@ export function Item({ data, number, onClick, publicItem }: ItemProps) {
         <div className={'relative flex justify-space w-full h-full gap-4 max-w-[800px] p-6 bg-[var(--clr-surface-a20)] rounded-xl'}
              onClick={() => onClick(number)}
         >
-            <img className={'w-[90px] rounded-xl'} alt={'sdf'} src={'./src/assets/book-icon.svg'}/>
+            <img className={'w-[90px] rounded-xl'} alt={'sdf'} src={'../assets/book-icon.svg'}/>
 
             <div className={'flex flex-col justify-between'}>
                 <div>{data.title}</div>
